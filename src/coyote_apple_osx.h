80c1e45cd42a6dedf2b2414039386679a68d3993
Ryan <rnleach@users.noreply.github.com>
2025-03-24 12:05:15 -0600

    Fix PATH_MAX on mac

--- src/coyote_apple_osx.h
+++ src/coyote_apple_osx.h
@@ -6,6 +6,7 @@
 // Apple / BSD specific implementation goes here - things NOT in common with Linux
 #include <sys/mman.h>
 #include <sys/resource.h>
+#include <sys/syslimits.h>
 #include <unistd.h>
 
 /* The reason for a seperate Linux and Apple impelementation is on Linux I can use the MAP_POPULATE flag, but I cannot on 
